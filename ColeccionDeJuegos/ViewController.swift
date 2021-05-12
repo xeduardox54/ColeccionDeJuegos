6bbfbaffc83eb20d729a9a39d56a79675979d00b
Eduardo Rodriguez <eduardo.rodriguez@tecsup.edu.pe>
2021-05-12 12:31:45 -0700

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Eduardo Justo Rodriguez Herrera on 5/12/21.
+//  Copyright © 2021 Eduardo Justo Rodriguez Herrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
