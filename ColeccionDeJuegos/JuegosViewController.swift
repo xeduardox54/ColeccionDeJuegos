b1c3e8af003f7df2bb72a269e11bb79dc17ac872
Eduardo Rodriguez <eduardo.rodriguez@tecsup.edu.pe>
2021-05-18 18:10:41 -0700

    Funcion de mostrar imagenes en Table View 1

---
+++ ColeccionDeJuegos/JuegosViewController.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+class JuegosViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var JuegoImagenView: UIImageView!
+    @IBOutlet weak var tituloTextField: UITextField!
+    
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+    }
+    
+    @IBAction func fotosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker,animated: true,completion:nil)
+    }
+    
+    @IBAction func camaraTapped(_ sender: Any) {
+    }
+    
+    @IBAction func agregarTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let juego = Juego(context:context)
+        juego.titulo = tituloTextField.text
+        juego.imagen = JuegoImagenView.image?.jpegData(compressionQuality:0.50)
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let imagenSeleccionada = info[.originalImage] as? UIImage
+        JuegoImagenView.image = imagenSeleccionada
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+}
